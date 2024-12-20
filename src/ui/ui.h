6b025e39087c8e396f68cd81d44ab3a4a479a65e
Goran Mahovlic <goran.mahovlic@gmail.com>
2024-12-20 15:02:11 +0100

    Cleanup

--- src/ui/ui.h
+++ src/ui/ui.h
@@ -15,7 +15,7 @@
 extern "C" {
 #endif
 
-extern const uint8_t assets[1923];
+extern const uint8_t assets[1928];
 
 void ui_init();
 void ui_tick();
--- src/ui/vars.h
+++ src/ui/vars.h
@@ -20,32 +20,32 @@ enum FlowGlobalVariables {
 
 // Native global variables
 
-extern int32_t get_var_motor_speed();
-extern void set_var_motor_speed(int32_t value);
-extern const char *get_var_current_operation();
-extern void set_var_current_operation(const char *value);
-extern int32_t get_var_current_position();
-extern void set_var_current_position(int32_t value);
 extern bool get_var_intial_home_z();
 extern void set_var_intial_home_z(bool value);
-extern double get_var_sensor();
-extern void set_var_sensor(double value);
 extern bool get_var_calibrated();
 extern void set_var_calibrated(bool value);
-extern double get_var_max_diff();
-extern void set_var_max_diff(double value);
 extern bool get_var_running();
 extern void set_var_running(bool value);
 extern bool get_var_positive_diff();
 extern void set_var_positive_diff(bool value);
-extern int32_t get_var_loops();
-extern void set_var_loops(int32_t value);
+extern double get_var_max_diff();
+extern void set_var_max_diff(double value);
+extern double get_var_sensor();
+extern void set_var_sensor(double value);
 extern double get_var_current_diff();
 extern void set_var_current_diff(double value);
 extern double get_var_sensor_min();
 extern void set_var_sensor_min(double value);
 extern double get_var_sensor_max();
 extern void set_var_sensor_max(double value);
+extern int32_t get_var_motor_speed();
+extern void set_var_motor_speed(int32_t value);
+extern int32_t get_var_current_position();
+extern void set_var_current_position(int32_t value);
+extern int32_t get_var_loops();
+extern void set_var_loops(int32_t value);
+extern const char *get_var_current_operation();
+extern void set_var_current_operation(const char *value);
 
 
 #ifdef __cplusplus
